f43f228c45febfb188f0c7ab6283f119519db987
Janlpz <jancar18@outlook.com>
2026-04-19 22:19:05 -0600

    Muestra informacion de un proyector usando ViewBag

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
+            //ViewBag es un objeto dinámico que permite enviar datos a la vista sin necesidad de definir una clase específica para ello
+
             //uso ViewData para enviar datos a la vista
             ViewData["Id"] = 1;
             ViewData["Marca"] = "Epson";
@@ -15,6 +17,12 @@ namespace WebApp.Controllers
             ViewData["Numero de serie"] = "123456";
             ViewData["Situacion"] = "Bueno";
             ViewData["FechaDeAlta"] = DateTime.Now.ToString();
+            ViewBag.Id = 2;
+            ViewBag.Marca = "Epson";
+            ViewBag.Modelo = "Xlight";
+            ViewBag.NumeroDeSerie = "123456";
+            ViewBag.Situacion = "Bueno";
+            ViewBag.FechaDeAlta = DateTime.Now.ToString();
             return View();
         }
     }
