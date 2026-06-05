ddb3fddde3213ea77513dbb70a4b4bd8cd204939
Janlpz <jancar18@outlook.com>
2026-06-05 11:38:55 -0600

    Realiza el sembrado de datos de Departamentos

--- WebApp/Data/AppDbContext.cs
+++ WebApp/Data/AppDbContext.cs
@@ -2,29 +2,88 @@
 using WebApp.Models;
 
 namespace WebApp.Data
+
 {
+
     public class AppDbContext : DbContext
     {
-        public DbSet<Proyector> Proyectores{ get; set; }
+
+        public DbSet<Proyector> Proyectores { get; set; }
 
         public DbSet<Departamento> Departamentos { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
-        { 
-        
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+
         }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
+
         {
+
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Departamento>()
-                .ToTable("Departamentos");
 
-            modelBuilder.Entity<Proyector>()
-                .ToTable("Proyectores");
+            modelBuilder.Entity<Departamento>().ToTable("Departamento").HasData(
+
+                new Departamento()
+
+                {
+                    Id = 1,
+                    Titulo = "DEPARTAMENTO DE SISTEMAS Y COMPUTACION"
+                },
+
+                new Departamento()
+
+                {
+                    Id = 2,
+                    Titulo = "DEPARTAMENTO DE INGENIERIA INDUSTRIAL"
+                },
+
+                new Departamento()
+
+                {
+                    Id= 3,
+                    Titulo = "DEPARTAMENTO DE CIENCIAS DE LA TIERRA"
+                },
+
+                new Departamento()
+
+                {
+                    Id = 4,
+                    Titulo = "DEPARTAMENTO DE CIENCIAS ECONOMICO-ADMINISTRATIVAS"
+                },
+
+                new Departamento()
+
+                {
+                    Id = 5,
+                    Titulo = "DEPARTAMENTO DE INGENIERIA METAL-MECANICA"
+                },
+
+                new Departamento()
+
+                {
+                    Id = 6,
+                    Titulo = "DEPARTAMENTO DE INGENIERIA QUIMICA Y BIOQUIMICA"
+                },
+
+                new Departamento()
+
+                {
+                    Id = 7,
+                    Titulo = "DEPARTAMENTO DE CIENCIAS BASICAS"
+                }
+
+            );
+
+            modelBuilder.Entity<Proyector>().ToTable("Proyectores");
+
         }
 
-        
     }
+
 }
---
+++ WebApp/Migrations/20260605173247_DataSeedDepartamentos.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedDepartamentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departamentos",
+                table: "Departamentos");
+
+            migrationBuilder.RenameTable(
+                name: "Departamentos",
+                newName: "Departamento");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departamento",
+                table: "Departamento",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Departamento",
+                columns: new[] { "Id", "Titulo" },
+                values: new object[,]
+                {
+                    { 1, "DEPARTAMENTO DE SISTEMAS Y COMPUTACION" },
+                    { 2, "DEPARTAMENTO DE INGENIERIA INDUSTRIAL" },
+                    { 3, "DEPARTAMENTO DE CIENCIAS DE LA TIERRA" },
+                    { 4, "DEPARTAMENTO DE CIENCIAS ECONOMICO-ADMINISTRATIVAS" },
+                    { 5, "DEPARTAMENTO DE INGENIERIA METAL-MECANICA" },
+                    { 6, "DEPARTAMENTO DE INGENIERIA QUIMICA Y BIOQUIMICA" },
+                    { 7, "DEPARTAMENTO DE CIENCIAS BASICAS" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departamento",
+                table: "Departamento");
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Departamento",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.RenameTable(
+                name: "Departamento",
+                newName: "Departamentos");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departamentos",
+                table: "Departamentos",
+                column: "Id");
+        }
+    }
+}
